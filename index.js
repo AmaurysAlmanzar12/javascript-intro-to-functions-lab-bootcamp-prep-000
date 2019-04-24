8c07a2fc5b55f00cb908a7bb0e7c297f953814ec
Amaurys Almanzar <amaurys.almanzar@wework.com>
2019-04-24 03:06:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string)
   return "I can't hear you!"
-}
-
-function sayHiToGrandma(string){
-  if (string.toUpperCase() === string)
+} else if (string.toUpperCase() === string){
   return "YES INDEED!"
+} else if (string === "I love you, Grandma.") {
+  return "I love you too."
+
 }
+  
